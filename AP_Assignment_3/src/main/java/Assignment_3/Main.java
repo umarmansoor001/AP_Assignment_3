0d962b33d9e6b2adf1bc221eb70317536fe7bb7e
umarmansoor001 <i190594@nu.edu.pk>
2021-11-19 15:24:50 +0500

    little effort

---
+++ AP_Assignment_3/src/main/java/Assignment_3/Main.java
@@ -0,0 +1,16 @@
+package Assignment_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for( i=0; i < args.length; ++i)
+		{
+			System.out.println(args[i]);
+		}
+		System.out.println("No. of files: " + i);
+	}
+
+}
