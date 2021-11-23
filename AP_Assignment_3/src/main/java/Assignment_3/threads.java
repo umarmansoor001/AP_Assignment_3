e3470be502363f45f26bf70961a1d01cd41becf4
umarmansoor001 <90560001+umarmansoor001@users.noreply.github.com>
2021-11-23 20:23:05 +0500

    Done with Assignment

--- AP_Assignment_3/src/main/java/Assignment_3/threads.java
+++ AP_Assignment_3/src/main/java/Assignment_3/threads.java
@@ -62,6 +62,7 @@ public class threads extends Thread {
 		//Creating FileReader and BufferedReader to read file 
 		FileReader FS;
 		BufferedReader bf;
+		//Critical section so making this part synchronized
 		synchronized (this)
 		{
 			try {
@@ -105,11 +106,17 @@ public class threads extends Thread {
 	public static void menu()
 	{
 		//Menu
+		System.out.println();
+		System.out.println("***************************************************************************");
+		System.out.println("*****************************   Menu   ************************************");
+		System.out.println("***************************************************************************");
 		System.out.println("Enter 1 to Display BST from Vocabulary file");
 		System.out.println("Enter 2 to Display Vectors from Input files");
 		System.out.println("Enter 3 to View match words and frequency");
 		System.out.println("Enter 4 to Search a query-> it should display all the files query found in");
 		System.out.println("Enter 5 to Exit");
+		System.out.println("***************************************************************************");
+		System.out.println("***************************************************************************");
 	}
 	//This method is use to check occurrence of words in Linked List 
 	public static boolean isalreadyPresent(LinkedList<word> L, String word)
@@ -121,9 +128,30 @@ public class threads extends Thread {
 		}
 		return false;//if word not found in whole Linked List then returns false so we need to make this word instance
 	}
+	//this method is use to find word object 
+	public static word find_word(LinkedList<word> L, String word)
+	{
+		for(word w: L)//Traversing Linked List using for each loop
+		{
+			if(w.get_word().equalsIgnoreCase(word))//if word found in Linked List then returns word
+				return w;
+		}
+		return  null;
+	}
+	//display all words and their frequencies
+	public static void display_words(LinkedList<word> L)
+	{
+		System.out.println("Word"+"	"+"Frequency");
+		for(word wl: L)
+		{
+			wl.display();
+		}	
+	}
+	//Driver method
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
+		//this list keep all match words
 		LinkedList<word> match_words=new LinkedList<word>();
 		
 		
@@ -145,7 +173,7 @@ public class threads extends Thread {
 		sleep(1000);
 		int choice = 0;
 		
-		
+		//making Scanner object to take input
 		Scanner input=new Scanner(System.in);
 
 		while(choice != 5)
@@ -155,6 +183,7 @@ public class threads extends Thread {
 				//calling menu function to display menu
 				threads.menu();
 				//taking input from user
+				System.out.println("Enter choice: ");
 				choice=input.nextInt();
 				//if user enters value out of limit then throw exception
 				if(choice < 0 || choice > 5)
@@ -173,44 +202,103 @@ public class threads extends Thread {
 						System.out.println(t[i].vs);
 					}
 				}	
-				else if(choice == 3)
+				else if(choice == 3)//this part presents words and their frequency 
 				{
-					//For Vector 1
-					for(String str: t[1].vs)//traversing vector 1
+					for(int i=1; i< args.length; i++)
 					{
-						for(String str_ts: t[0].ts)//Traversing BST 
+						//For Vectors
+						for(String str: t[i].vs)//traversing vector 1
 						{
-							if(str.equalsIgnoreCase(str_ts))//if words match each other
+							for(String str_ts: t[0].ts)//Traversing BST 
 							{
-								
-								if(!threads.isalreadyPresent(match_words, str))//if word's object is not present in linked list then need to create its object
-								{	
-									word w=new word(str);//make an instance of match word
-									match_words.add(w);//add that matched word instance in Linked list
+								if(str.equalsIgnoreCase(str_ts))//if words match each other
+								{
+									
+									if(!threads.isalreadyPresent(match_words, str))//if word's object is not present in linked list then need to create its object
+									{	
+										word w=new word(str);//make an instance of match word
+										match_words.add(w);//add that matched word instance in Linked list
+										break;//and leave loop to match another word
+									}
+									else 
+									{
+										threads.find_word(match_words, str).increment_frequency();//increment frequency in word
+										break;//and leave loop to match another word
+									}
+									
 								}
-								else 
-									//w.increment_frequency();
-								break;//and leave loop to match another word
 							}
 						}
 					}
-					//For Vector 2
-					for(String str: t[2].vs)//traversing vector 2
+					//After making matched words objects and calculating their frequencies 
+					//call display_words method
+					threads.display_words(match_words);
+				}
+				else if(choice == 4)
+				{
+					Scanner input_query=new Scanner(System.in);
+					System.out.print("Enter query:- ");
+					String query = input_query.nextLine();//taking string input
+					String[] query_words=query.split(" ");
+					LinkedList<word> matched_word=new LinkedList<word>();
+					for(int j=0; j < args.length; ++j)
 					{
-						for(String str_ts: t[0].ts)//Traversing BST
+						for(int i=0; i < query_words.length; ++i)
 						{
-							if(str.equalsIgnoreCase(str_ts))//if words match each other
+							//For vocabulary file
+							if(j==0)
 							{
-								if(!threads.isalreadyPresent(match_words, str))//if word's object is not present in linked list then need to create its object
-								{	
-									word w=new word(str);//make an instance of match word
-									match_words.add(w);//add that matched word instance in Linked list
+								for(String str: t[0].ts)
+								{
+									if(str.equalsIgnoreCase(query_words[i]))//if words match each other
+									{
+										
+										if(!threads.isalreadyPresent(matched_word, str))//if word's object is not present in linked list then need to create its object
+										{	
+											word w=new word(str);//make an instance of match word
+											matched_word.add(w);//add that matched word instance in Linked list
+										}
+										else 
+										{
+											threads.find_word(matched_word, str).increment_frequency();//increment frequency in word
+										}
+										
+									}
 								}
-								else 
-									//w.increment_frequency();
-								break;//and leave loop to match another word
+							}
+							//For Input files
+							else 
+							{
+								for(String str: t[j].vs)
+								{
+									if(str.equalsIgnoreCase(query_words[i]))//if words match each other
+									{
+										
+										if(!threads.isalreadyPresent(matched_word, str))//if word's object is not present in linked list then need to create its object
+										{	
+											word w=new word(str);//make an instance of match word
+											matched_word.add(w);//add that matched word instance in Linked list
+										}
+										else 
+										{
+											threads.find_word(matched_word, str).increment_frequency();//increment frequency in word
+										}
+										
+									}
+								}
 							}
 						}
+						System.out.print("In ");
+						System.out.println(t[j].get_filename()+" File");
+						System.out.println("Only "+ matched_word.size() +" word matches");
+						if(matched_word.size()!=0)
+						{
+							System.out.println("Number of matched words and their Frequencies.");
+							threads.display_words(matched_word);
+							System.out.println();
+							matched_word.clear();//making list empty
+						}
+						
 					}
 				}
 				else if(choice == 5)
@@ -221,10 +309,6 @@ public class threads extends Thread {
 				System.out.println(e);
 			}
 		}
-		for(word wl: match_words)
-		{
-			
-			System.out.println(wl.get_word());
-		}
+		
 	}
 };
